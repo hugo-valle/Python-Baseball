01f0e0dce61f64c191753f4445459539d879f3ab
Hugo Valle <hugovalle1@weber.edu>
2019-01-30 21:35:54 -0700

    mod4

--- stats/offense.py
+++ stats/offense.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
-plays.columns = ['type', 'inning', 'team', 'player', 'count', 'piches',
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches',
         'event', 'game_id', 'year']
 
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),
